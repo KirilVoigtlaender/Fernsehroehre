bb2461e865947c055fed165246ed144a995e21a5
ksdb12 <87563681+ksdb12@users.noreply.github.com>
2021-08-21 14:49:15 +0200

    Steuerung der Magnetfeldstärke

--- Magnet.java
+++ Magnet.java
@@ -11,11 +11,26 @@ public class Magnet extends Actor
     private Vektor Lage;
     private double abstand;
     private GreenfootImage image=new GreenfootImage(1200,800);
-    public Magnet(Vektor v, double abstand)
+    private double magnetfeldstärke = 0;
+    private String name;
+    private String erhöhen;
+    private String verringern;
+    private double maxBetrag;
+    
+    private int y;
+    public Magnet(Vektor v, double abstand,  int y ,String name, String erhöhen, String verringern, double maxBetrag)
     {
-        Lage=v;
-        this.abstand=abstand;
+        Lage = v;
+        this.abstand = abstand;
+        
+        this.y = y;
+        this.name = name;
+        this.erhöhen = erhöhen;
+        this.verringern = verringern;
+        this.maxBetrag = maxBetrag;
+        
         draw();
+        
     }
     /**
      * Act - do whatever the Magnet wants to do. This method is called whenever
@@ -24,18 +39,42 @@ public class Magnet extends Actor
     public void act()
     {
         draw();
+        steuern();
     }
     public void draw()
     {
+        image.clear();
         Bildpunkt Pol1 = MyWorld.perspective(Lage.multiplizieren(abstand/2));
         image.setColor(Color.RED);
-        image.drawOval(Pol1.x-10,Pol1.y-10,20,20);
-        //setImage(image);
+        image.fillOval(Pol1.x-10,Pol1.y-10,20,20);
+        
         Bildpunkt Pol2 = MyWorld.perspective(Lage.multiplizieren(-abstand/2));
         image.setColor(Color.BLUE);
-        image.drawOval(Pol2.x-10,Pol2.y-10,20,20);
+        image.fillOval(Pol2.x-10,Pol2.y-10,20,20);
         image.setColor(Color.ORANGE);
         image.drawLine(Pol1.x,Pol1.y,Pol2.x,Pol2.y);
+        image.setColor(Color.BLACK);
+        image.drawString(name + ": "+ String.valueOf(magnetfeldstärke),10,y);
         setImage(image);
+    }
+    public void steuern()
+    {
+        
+        if(Greenfoot.isKeyDown(erhöhen))
+        {
+            magnetfeldstärke += 0.1;
+            if(magnetfeldstärke > maxBetrag)
+            {
+                magnetfeldstärke = maxBetrag;
+            }
+        }
+        if(Greenfoot.isKeyDown(verringern))
+        {
+           magnetfeldstärke -= 0.1;
+           if(magnetfeldstärke < -maxBetrag)
+            {
+                magnetfeldstärke = -maxBetrag;
+            }
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -19,7 +19,8 @@ public class MyWorld extends World
         super(1200, 800, 1); 
         //image.drawLine(100,70,10,18);
         //setBackground(image);
-        
+        image.setColor(Color.WHITE);
+        image.fill();
         Bildpunkt B1 = perspective(new Vektor(-200,-80,-80));
         //B1.koordinatenAusgeben(image);
         Bildpunkt B2 = perspective(new Vektor(-200,-80,80));
@@ -38,6 +39,7 @@ public class MyWorld extends World
         Bildpunkt B10 = perspective(new Vektor(600,-200,-200));
         Bildpunkt B11 = perspective(new Vektor(600,200,200));
         Bildpunkt B12 = perspective(new Vektor(600,200,-200));
+        image.setColor(Color.BLACK);
         image.drawLine(B9.x,B9.y,B11.x,B11.y);
         image.drawLine(B9.x,B9.y,B10.x,B10.y);
         image.drawLine(B11.x,B11.y,B6.x,B6.y);
@@ -61,8 +63,8 @@ public class MyWorld extends World
         image.drawLine(B12.x,B12.y,B10.x,B10.y);
         setBackground(image);
         
-        addObject(new Magnet(new Vektor(0,0,1),140),600,400);
-        addObject(new Magnet(new Vektor(0,1,0),140),600,400);
+        addObject(new Magnet(new Vektor(0,0,1),140,20,"senkrecht","up","down",30),600,400);
+        addObject(new Magnet(new Vektor(0,1,0),140,50,"waagerecht","right","left",30),600,400);
     }
     
     public static Bildpunkt perspective(Vektor v)
