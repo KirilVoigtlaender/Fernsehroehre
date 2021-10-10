c446886e44eca41b307e4a1c856b7441ee96387c
ksdb12 <87563681+ksdb12@users.noreply.github.com>
2021-10-10 14:10:15 +0200

    Varibel Umbennenung

--- Fernsehröhre.java
+++ Fernsehröhre.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Fernsehröhre extends World
 {
-    private GreenfootImage image = new GreenfootImage(1200,800);
+    private GreenfootImage background = new GreenfootImage(1200,800);
     private GreenfootImage foreground = new GreenfootImage(1200,800);
     /**
      * Constructor for objects of class Fernsehröhre.
@@ -22,33 +22,33 @@ public class Fernsehröhre extends World
         double bildschirmbreite = 220;
         double bildschirmhöhe = 220;
         double röhrendicke = 100;
-        image.setColor(Color.WHITE);
-        image.fill();
-        image.setColor(Color.BLACK);
+        background.setColor(Color.WHITE);
+        background.fill();
+        background.setColor(Color.BLACK);
         Bildpunkt B1 = perspective(new Vektor(-200,-röhrendicke,-röhrendicke));
-        B1.koordinatenAusgeben(image);
+        B1.koordinatenAusgeben(background);
         Bildpunkt B2 = perspective(new Vektor(-200,-röhrendicke,röhrendicke));
-        B2.koordinatenAusgeben(image);
+        B2.koordinatenAusgeben(background);
         Bildpunkt B3 = perspective(new Vektor(-200,röhrendicke,röhrendicke));
-        B3.koordinatenAusgeben(image);
+        B3.koordinatenAusgeben(background);
         Bildpunkt B4 = perspective(new Vektor(-200,röhrendicke,-röhrendicke));
-        B4.koordinatenAusgeben(image);
+        B4.koordinatenAusgeben(background);
         Bildpunkt B5 = perspective(new Vektor(150,röhrendicke,-röhrendicke));
-        B5.koordinatenAusgeben(image);
+        B5.koordinatenAusgeben(background);
         Bildpunkt B6 = perspective(new Vektor(150,röhrendicke,röhrendicke));
-        B6.koordinatenAusgeben(image);
+        B6.koordinatenAusgeben(background);
         Bildpunkt B7 = perspective(new Vektor(150,-röhrendicke,-röhrendicke));
-        B7.koordinatenAusgeben(image);
+        B7.koordinatenAusgeben(background);
         Bildpunkt B8 = perspective( new Vektor(150,-röhrendicke,röhrendicke));
-        B8.koordinatenAusgeben(image);
+        B8.koordinatenAusgeben(background);
         Bildpunkt B9 = perspective( new Vektor(bildschirmabstand,-bildschirmbreite,bildschirmhöhe));
-        B9.koordinatenAusgeben(image);
+        B9.koordinatenAusgeben(background);
         Bildpunkt B10 = perspective(new Vektor(bildschirmabstand,-bildschirmbreite,-bildschirmhöhe));
-        B10.koordinatenAusgeben(image);
+        B10.koordinatenAusgeben(background);
         Bildpunkt B11 = perspective(new Vektor(bildschirmabstand,bildschirmbreite,bildschirmhöhe));
-        B11.koordinatenAusgeben(image);
+        B11.koordinatenAusgeben(background);
         Bildpunkt B12 = perspective(new Vektor(bildschirmabstand,bildschirmbreite,-bildschirmhöhe));
-        B12.koordinatenAusgeben(image);
+        B12.koordinatenAusgeben(background);
         
         foreground.drawPolygon(new int []{B3.x,B4.x,B1.x,B7.x,B10.x,B9.x,B11.x,B6.x},
         new int[]{B3.y,B4.y,B1.y,B7.y,B10.y,B9.y,B11.y,B6.y},8);
@@ -60,14 +60,14 @@ public class Fernsehröhre extends World
         foreground.drawLine(B8.x,B8.y,B6.x,B6.y);
         addObject(new Gehäuse(foreground),600,400);
         
-        image.setColor(Color.LIGHT_GRAY);
-        image.drawLine(B5.x,B5.y,B6.x,B6.y);
-        image.drawLine(B5.x,B5.y,B7.x,B7.y);
-        image.drawLine(B5.x,B5.y,B4.x,B4.y);
-        image.drawLine(B12.x,B12.y,B5.x,B5.y);
-        image.drawLine(B12.x,B12.y,B11.x,B11.y);
-        image.drawLine(B12.x,B12.y,B10.x,B10.y);
-        setBackground(image);
+        background.setColor(Color.LIGHT_GRAY);
+        background.drawLine(B5.x,B5.y,B6.x,B6.y);
+        background.drawLine(B5.x,B5.y,B7.x,B7.y);
+        background.drawLine(B5.x,B5.y,B4.x,B4.y);
+        background.drawLine(B12.x,B12.y,B5.x,B5.y);
+        background.drawLine(B12.x,B12.y,B11.x,B11.y);
+        background.drawLine(B12.x,B12.y,B10.x,B10.y);
+        setBackground(background);
         Leuchtschirm L = new Leuchtschirm(bildschirmbreite,bildschirmhöhe,280,"delete");
         addObject(L,600,400);
         Elektronenkanone E = new Elektronenkanone(new Vektor(1,0,0),-180,35,205,"Elektronenkanone","+","-",12,16);
